ae2752a8641bbcc2516f9e1805528e80216bad0c
1Gift3 <joshuaramaoka@gmail.com>
2025-10-02 17:42:33 +0200

    second part of conditionals

---
+++ week1/excercises 3.0 conditionals/3.2.py
@@ -0,0 +1,19 @@
+score = input("Enter score between 0.0 and 1.0: ")
+score = float(score)
+if score >= 0.9 and score <= 1.0:
+    print("A")
+
+elif score >= 0.8 and score < 0.9:
+    print("B")
+
+elif score >= 0.7 and score < 0.8:
+    print("C")
+
+elif score >= 0.6 and score < 0.7:
+    print("D")
+
+elif score < 0.6 and score >= 0.0:
+    print("F")
+
+else: 
+    print("Error, please enter a number between 0.0 and 1.0")
