967dbc0e11d3477c00886815443f0d28d6ea9185
1Gift3 <joshuaramaoka@gmail.com>
2025-10-28 15:03:26 +0200

    pdf automation script

---
+++ automation/pdf_folder.py
@@ -0,0 +1,28 @@
+import os
+import errno
+
+#defining  source and target folders
+source = "C:/Users/joshu/Downloads"
+target = "C:/Users/joshu/Downloads/Downloaded_pdfs"
+
+# Creating target folder if not there (compatible with older Python)
+try:
+    os.makedirs(target)
+except OSError as e:
+    # If the directory already exists, ignore the error; otherwise re-raise
+    if e.errno != errno.EEXIST:
+        raise
+
+#loop through downloads folder
+for file in os.listdir(source):
+    #check for image file extensions
+    if file.lower().endswith((".pdf")):
+        #defining full file paths
+        src_path = os.path.join(source, file)
+        dest_path = os.path.join(target, file)
+
+        #move file
+        os.rename(src_path, dest_path)
+        print(f'Moved: {file}')
+
+print ('All images have been moved to Downloaded_images folder.')
